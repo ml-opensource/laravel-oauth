79a72abe881a46bf3905235bc9e67a7f12262f10
Simon Yousoufov <syousoufov@fuzzproductions.com>
2016-02-24 11:13:45 -0500

    Allow policy to require scopes

--- src/Policies/RepositoryModelPolicy.php
+++ src/Policies/RepositoryModelPolicy.php
@@ -4,10 +4,35 @@ namespace Fuzz\Auth\Policies;
 
 use Fuzz\Auth\Models\Traits\ChecksScopes;
 use Fuzz\MagicBox\Contracts\Repository;
+use League\OAuth2\Server\Exception\AccessDeniedException;
 
 abstract class RepositoryModelPolicy implements RepositoryModelPolicyInterface
 {
 	use ChecksScopes;
+
+	/**
+	 * Require a set of scopes or throw exception
+	 *
+	 * Scopes passed as an array in one argument are all required. Of scopes that are passed as separate arguments,
+	 * only one set is required.
+	 *
+	 * $args = ['IAmRequired', 'MeToo'], ['orUs', 'andUsToo']
+	 *
+	 * @return bool
+	 * @throws \League\OAuth2\Server\Exception\AccessDeniedException
+	 */
+	public function requireScopes()
+	{
+		$scopes = func_get_args();
+
+		$has_scopes = call_user_func_array([$this, 'hasOneOfScopes'], $scopes);
+
+		if (! $has_scopes) {
+			throw new AccessDeniedException;
+		}
+
+		return true;
+	}
 
 	/**
 	 * Merge new filters with the existing repository filters
